088aa520f2472314300cd156f5571d6a9870a0a3
TMN-関 将史 <masashi.seki@tm-nets.com>
2019-05-27 14:34:22 +0900

    プロジェクト ファイルを追加します。

---
+++ TsvClipper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace TsvClipper
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            if (args.Length != 1) return;
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("ファイルが存在しません");
+                return;
+            }
+            using (var sr = new StreamReader(args[0]))
+            {
+                var text = sr.ReadToEnd().Replace(",", "\t");
+                Clipboard.SetText(text);
+            }
+        }
+    }
+}
